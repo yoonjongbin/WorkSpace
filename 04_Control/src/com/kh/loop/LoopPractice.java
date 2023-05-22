276354dbde7d00fd74adb65636b4e4fd11339c2e
yoonjongbin <133620288+yoonjongbin@users.noreply.github.com>
2023-05-22 09:03:31 +0900

    Update LoopPractice.java

--- 04_Control/src/com/kh/loop/LoopPractice.java
+++ 04_Control/src/com/kh/loop/LoopPractice.java
@@ -11,8 +11,9 @@ class LoopPractice {
 		//p.method1();
 		//p.method2();
 		//p.method3();
-		p.method4();
-		
+		//p.method4();
+		//p.method5();
+		p.method6();
 		
 	}
 	
@@ -40,21 +41,22 @@ class LoopPractice {
 
     // 1+(-2)+3+(-4)+...과 같은 식으로 계속 더해나갔을 때, 몇까지 더해야 총합이 100 이상 되는지 출력하시오.
     public void method2() {
-    	int count = 1;
-    	int minus = 0;
+    	int num = 1;
+    	//int minus = 0;
     	int sum  = 0;
     	
     	while(true) {
     		
-    		if(count % 2 == 0) {
-    			minus = -count;
-    			sum += minus;
+    		if(num % 2 == 0) {
+    			//minus = -count;
+    			sum -= num;
     			
-    		} else sum += count;
+    		} else sum += num;
     		if(sum >= 100) break;
-    		count++;
+    		
+    		num++;
     	}
-    	System.out.println(count + "회 더해야 100이상이 됩니다.");
+    	System.out.println(num + "회 더해야 100이상이 됩니다.");
     	
     	
     	
@@ -72,7 +74,7 @@ class LoopPractice {
     	
     	String str = "";
     	char index;
-    	int count = 0;
+    	int ch_Same = 0;
     	
     	System.out.print("문자열 : ");
     	str = sc.nextLine();
@@ -83,11 +85,11 @@ class LoopPractice {
     	
     	for(int i = 0; i < str.length(); i++) {
     		if(str.charAt(i) == index) {
-    			count++;
+    			ch_Same++;
     		}else continue;
     	}
     	
-    	System.out.printf("%s 안에 포함된 %c 개수 : %d", str, index, count);
+    	System.out.printf("%s 안에 포함된 %c 개수 : %d개", str, index, ch_Same);
     	
     }
 
@@ -126,15 +128,37 @@ class LoopPractice {
      */
     public void method5() {
     	int random = 0;
-    	int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
+    	int d1 = 0, d2 = 0, d3 = 0, d4 = 0, d5 = 0, d6 = 0;
     	
     	for(int i = 0; i < 10; i++) {
-    		random = (int)(Math.random() * 10);
+    		
+    		random = (int)(Math.random() * 6) + 1;
+    		
     		switch(random) {
-    			case 1: a1++;
+    			case 1: 
+    				d1++;
+    				break;
+    			case 2: 
+    				d2++;
+    				break;
+    			case 3: 
+    				d3++;
+    				break;
+    			case 4: 
+    				d4++;
+    				break;
+    			case 5: 
+    				d5++;
+    				break;
+    			case 6: 
+    				d6++;
+    				break;
     				
     		}
+    	
     	}
+    	System.out.printf("1 : %d\n" + "2 : %d\n" + "3 : %d\n" + "4 : %d\n" + "5 : %d\n" + "6 : %d", 
+    						d1, d2, d3, d4, d5, d6);
     }
 
     /*
@@ -160,7 +184,98 @@ class LoopPractice {
         이겼습니다 !
     */
     public void method6() {
+    	
+    	boolean win = false;
+    	
+    	
+    	String name = "";
+    	String player = "";
+    	String com = "";
+    	
+    	int count;
+    	
+    	int win_C = 0;
+    	int lose_C = 0;
+    	int draw_C = 0;
+    	
+    	
+    	System.out.print("당신의 이름을 입력해주세요 : ");
+    	name = sc.nextLine();
+    	
+    	for(count = 1; win == false; count++) {
+    		
+    		int random = (int)(Math.random() * 3) + 1;
+    		
+    		
+    		System.out.print("가위바위보 : ");
+    		player = sc.nextLine();
+    		
+    		
+    		
+    		
+    		
+    		switch(random) {
+    			case 1 :
+    				com = "가위";
+    				break;
+    			case 2 :
+    				com = "바위";
+    				break;
+    			case 3 :
+    				com = "보";
+    				break;
+    		}
+    		
+    		System.out.printf("%s : %s\n" + "컴퓨터 : %s\n", name, player, com);
+    		
+    		if(player.equals("가위") || player.equals("바위") || player.equals("보")) {
+    			
+    			if(player.equals(com)) {
+    				System.out.println("비겼습니다.\n");
+    				draw_C++;
+    			}
+    			else if(player.equals("가위") && com.equals("보")) {
+    				System.out.println("이겼습니다!!\n");
+    				win_C++;
+    				
+    				break;
+    				
+    			} else if(player.equals("바위") && com.equals("가위")) {
+    				System.out.println("이겼습니다!!\n");
+    				win_C++;
+    				
+    				break;
+    				
+    			} else if(player.equals("보") && com.equals("바위")) {
+    				System.out.println("이겼습니다!!\n");
+    				win_C++;
+    				
+    				break;
+    				
+    			} else if(player.equals("가위") && com.equals("바위")) {
+    				System.out.println("졌습니다 ㅠㅠ\n");
+    				lose_C++;
+    				
+    			} else if(player.equals("바위") && com.equals("보")) {
+    				System.out.println("졌습니다 ㅠㅠ\n");
+    				lose_C++;
+    				
+    			} else if(player.equals("보") && com.equals("가위")) {
+    				System.out.println("졌습니다 ㅠㅠ\n");
+    				lose_C++;
+    				
+    			}
 
+    		} else {
+    			
+    			System.out.println("가위, 바위, 보 중 하나만 내주세요.\n");
+    			count--;
+    			
+    		}
+    		
+    	}
+    	System.out.printf("시도한 횟수는 %d번이고, 승리한 횟수 %d번, 패배한 횟수 %d번, 무승부한 횟수 %d번입니다.", count, win_C, lose_C, draw_C);
+    	
     }
 
 }
